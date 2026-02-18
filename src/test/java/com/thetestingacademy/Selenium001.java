cf34c2956d8136b948c40999ed84c757db9f1d0e
MISMI RAJASHREE SATPATHY <mismirajashree@gmail.com>
2026-02-18 12:10:40 +0530

    First code Selenium. Opening Google.com in Chrome using Selenium

--- src/test/java/com/thetestingacademy/Selenium001.java
+++ src/test/java/com/thetestingacademy/Selenium001.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Selenium001 {
     public static void main(String[] args) {
+        System.out.println("Opening GoogleChrome");
         ChromeDriver driver=new ChromeDriver();
         driver.get("https://www.google.com");
     }
